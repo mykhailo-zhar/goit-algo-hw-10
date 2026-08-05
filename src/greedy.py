1e518205e59388bec01f135aaa01dae7ece20544
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-08-05 12:25:13 +0300

    Add memorized recursive algorithm

--- src/greedy.py
+++ src/greedy.py
@@ -22,31 +22,49 @@ def find_coins_greedy(coins: list[int], sum: int) -> dict[int, int]:
     return result
 
 
-def _min_coins(coins: list[int], sum: int, coin_index):
+def _min_coins_recursive(coins: list[int], sum: int, coin_index):
     if sum == 0:
         return [0, []]
 
     if sum < 0 or coin_index == len(coins):
         return [float("inf"), []]
 
-    take = _min_coins(coins, sum - coins[coin_index], coin_index)
-    not_take = _min_coins(coins, sum, coin_index + 1) or []
+    take = _min_coins_recursive(coins, sum - coins[coin_index], coin_index)
+    not_take = _min_coins_recursive(coins, sum, coin_index + 1) or []
     if take[0] != float("inf"):
-        take[0] += 1
         take[1].append(coins[coin_index])
+        take[0] = len(take[1])
 
     return min(take, not_take, key=lambda item: item[0])
 
 
+def _min_coins_recursive_memo(coins: list[int], sum: int, coin_index: int, memo: dict):
+    if sum == 0:
+        return [0, []]
+
+    if sum < 0 or coin_index == len(coins):
+        return [float("inf"), []]
+
+    if memo[coin_index][sum] != -1:
+        return memo[coin_index][sum]
+
+    take = _min_coins_recursive_memo(coins, sum - coins[coin_index], coin_index, memo)
+    no_take = _min_coins_recursive_memo(coins, sum, coin_index + 1, memo)
+    if take[0] != float("inf"):
+        take[1].append(coins[coin_index])
+        take[0] = len(take[1])
+
+    min_value = min(take, no_take, key=lambda item: item[0])
+    memo[coin_index][sum] = [min_value[0], min_value[1][:]]
+    return memo[coin_index][sum]
+
+
 def find_min_coins(coins: list[int], sum: int) -> dict[int, int]:
-    # Задача полягає у знайденні мінімальної суми монет різного номіналу
-    # для видачі певної суми
 
-    # Мінімізація кількості монет - пошук максимальної кількості монет номіналу, що задовольнить суму
-    # Беремо монету
-    # Якщо можемо відняти - віднімаємо
-    # Не можем - берем наступну
-
-    result = Counter(_min_coins(coins, sum, 0)[1])
+    # result = Counter(_min_coins_recursive(coins, sum, 0)[1])
+    memo = [[-1] * (sum + 1) for _ in range(len(coins))]
+    result = _min_coins_recursive_memo(coins, sum, 0, memo)
+    print(result[0])
+    result = Counter(result[1])
 
     return dict(result)

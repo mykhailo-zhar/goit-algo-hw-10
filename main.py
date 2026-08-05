f267cef594b98854496ea350a2c41076acb27203
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-08-05 14:33:39 +0300

    Add both scripts to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from TODO: replace goit-pycore-template!")
-
+from src.greedy import benchmark
+from src.monte_carlo import main
 
 if __name__ == "__main__":
+    print("Coin change реалізований жадібним алгоритмом та динамічним програмуванням")
+    benchmark()
+
+    print("=" * 30, "\n" * 2)
     main()
